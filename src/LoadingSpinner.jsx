a7f83d33c2567c41391a83b30bc5a202f24d264d
tirthankar-halder <tirthankar.halder@gmail.com>
2025-04-02 00:06:27 -0400

    Create LoadingSpinner.jsx

---
+++ src/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingSpinner.css"; // For styling
+
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
